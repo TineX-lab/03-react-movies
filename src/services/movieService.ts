576c190e3d290be98a4cead7cc7e5945dd5a8b50
Oleksandr <alexsandrolegov@gmail.com>
2026-09-13 21:13:26 +0200

    Refactor

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
-import { type MoviesResponse } from "../types/movie";
+import { type Movie } from "../types/movie";
 
 const token = import.meta.env.VITE_TMDB_TOKEN;
+
+export interface MoviesResponse {
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+  page: number;
+}
 
 const movieClient = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
